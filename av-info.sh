e2f09351f68f35188d5178f3771fb082bead028c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2021-12-13 15:14:21 -0600

    added DATE and some cleanup

--- av-info.sh
+++ av-info.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+ #!/bin/bash
 
 file=${1}
 
-ffmpeg -i "${file}" 2>&1 | grep --color=always "Stream #0:.*:\|Duration:*"
+ffmpeg -i "${file}" 2>&1 | grep -P --color=always "DATE.*?:|Duration:*|Stream #0:.*?:"
