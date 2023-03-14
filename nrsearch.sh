79f1f5719d1d810bcd70a8d4b178d7140fac5e71
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2023-03-14 13:09:47 -0500

    updates

--- nrsearch.sh
+++ nrsearch.sh
@@ -4,4 +4,4 @@ query="$@"
 
 find $HOME/.tmp/nr/ -type d -iname "*${query}*"
 
-find $HOME/.tmp/nr/ -type f -name info -exec bash -c "if grep ${query} {} >/dev/null; then echo -e '{}'; fi" \;
+find $HOME/.tmp/nr/ -type f -name info -exec bash -c "if grep --ignore-case ${query} {} >/dev/null; then echo -e '{}'; fi" \;
