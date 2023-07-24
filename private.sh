490f8e3044917ef998eda84282dbbece48494ec8
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2023-07-24 15:18:38 -0500

    generalized

--- private.sh
+++ private.sh
@@ -7,10 +7,10 @@ if [ -z "$arg" ]; then
         sig=$(printf "%s" "${passphrase}" | ecryptfs-add-passphrase - | sed 's/\(.*\)\[\(.*\)\]\(.*\)/\2/g')
         # Old mount options
         #sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig="${sig}",ecryptfs_sig="${sig}",ecryptfs_cipher=aes,sig_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" ~/.e ~/.private
-        sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig="${sig}",ecryptfs_sig="${sig}",ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" ~/.e ~/.private
+        sudo mount -t ecryptfs -o rw,relatime,ecryptfs_fnek_sig="${sig}",ecryptfs_sig="${sig}",ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_unlink_sigs,verbosity=0,key=passphrase:passphrase_passwd="$passphrase" .e .private
 else
         if [ "$arg" == "-u" ]; then
-                sudo umount ~/.private
+                sudo umount .private
                 keyctl list @u
                 keyctl clear @u
         else
