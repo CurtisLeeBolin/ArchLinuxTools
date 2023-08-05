c6193ffde2e207f0085b43397dc12cd5798e0198
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2023-08-05 15:55:52 -0500

    updated

--- update-avtc.sh
+++ update-avtc.sh
@@ -5,8 +5,17 @@ if [[ ! -f $(which git 2>/dev/null) ]]; then
   sudo pacman -Sy --noconfirm --needed git
 fi
 
-if [ -d ~/Projects/avtc/ ]; then
-  python -m pip install ~/Projects/avtc/
-else
-  python -m pip install git+https://github.com/CurtisLeeBolin/avtc.git
+
+if [ ! -d ~/.local/lib/avtc/ ]; then
+  python -m venv ~/.local/lib/avtc
 fi
+
+~/.local/lib/avtc/bin/python -m pip install --upgrade pip
+
+if [ -d ~/Projects/avtc/ ]; then
+  ~/.local/lib/avtc/bin/python -m pip install ~/Projects/avtc/
+else
+  ~/.local/lib/avtc/bin/python -m pip install git+https://github.com/CurtisLeeBolin/avtc.git
+fi
+
+cp ~/.local/lib/avtc/bin/avtc ~/.local/bin/
