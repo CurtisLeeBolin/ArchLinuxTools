788b42151283a638454d23c8ad235e0b4412c608
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2023-12-12 13:42:59 -0600

    Updated

---
+++ nrdl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/.local/lib/nrdl/bin/cyberdrop-dl --config-file ~/.config/nrdl/settings.yaml
---
+++ rngdirname.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+print_help () {
+  echo "rngdirname - renames dirs to random strings"
+  echo
+  echo "'-h, --help' Prints this help."
+}
+
+d=2
+if [ "${1}" == "-h" ] || [ "${1}" == "--help" ]; then
+  print_help
+  exit 0
+fi
+
+i=${2}
+for f in */; do
+  random_string=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 13)
+  echo mv "${f}" "${random_string}"
+  mv "${f}" "${random_string}"
+done
+
+
+
+
