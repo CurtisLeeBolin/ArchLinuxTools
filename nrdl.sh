713a83e7723849d279d397df70c25fb1c0e16382
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2024-07-23 19:55:18 -0500

    Added argument to retry failed

--- nrdl.sh
+++ nrdl.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-~/.local/lib/nrdl/bin/cyberdrop-dl --config-file ~/.config/nrdl/settings.yaml
+if [ -z "$1" ]; then
+  ~/.local/lib/nrdl/bin/cyberdrop-dl --config-file ~/.config/nrdl/settings.yaml
+else
+  if [ "${1}" == "-r" ]; then
+    ~/.local/lib/nrdl/bin/cyberdrop-dl --retry-failed --config-file ~/.config/nrdl/settings.yaml
+  else
+    echo 'Error: Invalid Argument'
+    exit 1
+  fi
+fi
