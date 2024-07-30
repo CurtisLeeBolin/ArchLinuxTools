050ca0e4516ce8099c4b1976e7b7c94509587421
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2024-07-30 11:32:26 -0500

    Work around cyberdrop-dl bug on retry

--- nrdl.sh
+++ nrdl.sh
@@ -4,7 +4,11 @@ if [ -z "$1" ]; then
   ~/.local/lib/nrdl/bin/cyberdrop-dl --config-file ~/.config/nrdl/settings.yaml
 else
   if [ "${1}" == "-r" ]; then
-    ~/.local/lib/nrdl/bin/cyberdrop-dl --retry-failed --config-file ~/.config/nrdl/settings.yaml
+    #~/.local/lib/nrdl/bin/cyberdrop-dl --retry-failed --config-file ~/.config/nrdl/settings.yaml
+    while IFS=', ' read -r url trash; do
+      dl "${url}"
+      sleep 5s
+    done < Download_Error_URLs.csv
   else
     echo 'Error: Invalid Argument'
     exit 1
