836881fe95c8abfa4906edab31bb7f2896b49e90
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-01-08 22:56:07 -0600

    Added update-streamlink

---
+++ update-streamlink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# update-streamlink.sh
+
+if [[ ! -f $(which git 2>/dev/null) ]]; then
+  sudo pacman -Sy --noconfirm --needed git
+fi
+
+if [ ! -d ~/.local/lib/streamlink/ ]; then
+  python -m venv ~/.local/lib/streamlink
+fi
+
+~/.local/lib/yt-dlp/bin/python -m pip install --upgrade pip
+
+#~/.local/lib/streamlink/bin/python -m pip install ~/Projects/streamlink/
+~/.local/lib/streamlink/bin/python -m pip install git+https://github.com/streamlink/streamlink.git
+
+cp ~/.local/lib/streamlink/bin/streamlink ~/.local/bin/
