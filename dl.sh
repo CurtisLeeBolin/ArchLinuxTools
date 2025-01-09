0241f1bc6f42a5a7feedaf70864862e064a943f5
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-01-08 21:53:44 -0600

    Added `--adjust-extension`

--- dl.sh
+++ dl.sh
@@ -3,5 +3,5 @@
 USER_AGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
 
 for i in ${@}; do
-  wget --continue --user-agent="${USER_AGENT}" "${i}"
+  wget --adjust-extension --continue --user-agent="${USER_AGENT}" "${i}"
 done
