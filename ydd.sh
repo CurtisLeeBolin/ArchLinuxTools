33f2b64757665d74295915706baffebfb9a8084b
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-06-29 01:52:31 -0500

    rm ydd.sh

--- ydd.sh
+++ ydd.sh
@@ -3,28 +3,31 @@
 # Downloads all videos of a channel/user/playlist or list of videos with date
 # ydd <link> <link> ...
 
-url="$@"
+command_array=(
+  'yt-dlp'
+  '--sub-lang' 'en,id,-live_chat'
+  '--embed-subs'
+  '--embed-metadata'
+  '--output' "%(upload_date)s %(uploader)s - %(title)s [%(id)s].%(ext)s"
+)
 
-download_video () {
-	yt-dlp \
-  --sub-langs en,id,-live_chat \
-  --embed-subs \
-  --embed-metadata \
-  --output "%(upload_date)s %(uploader)s - %(title)s [%(id)s].%(ext)s"  \
-  $2 \
-  $1
-}
-
-cookies=""
 if [ -f cookies.txt ]; then
-  cookies="--cookies cookies.txt"
+  command_array+=('--cookies' 'cookies.txt')
+fi
+
+if [[ "$1" == '-l' ]]; then
+  command_array+=('--format' 'bv[height<=1080]+ba/b[height<=1080]')
+  shift
+  url="$@"
+else
+  url="$@"
 fi
 
 if [[ "$url" =~ ( |\') ]]; then
   arr=(${url})
   for each in "${arr[@]}"; do
-    download_video ${each} "${cookies}"
+    "${command_array[@]}" ${each}
   done
 else
-  download_video ${url} "${cookies}"
+  "${command_array[@]}" ${url}
 fi
