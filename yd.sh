c558a538327d7ab98051ffa9b5ec8284a04ad490
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-07-30 13:17:01 -0500

    Updated yd

--- yd.sh
+++ yd.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 # yd
-# Downloads all videos of a channel/user/playlist or list of videos
+# Downloads all videos of a channel/user/playlist or list of videos with date
 # yd <link> <link> ...
 
-url="$@"
+command_array=(
+  'yt-dlp'
+  '--sub-lang' 'en,id,-live_chat'
+  '--embed-subs'
+  '--embed-metadata'
+  '--live-from-start'
+  '--output' "%(uploader)s - %(title)s [%(id)s].%(ext)s"
+)
 
-#download_video () {
-#	while true; do
-# 		yt-dlp \
-#      --sub-langs en,id,-live_chat \
-#      --embed-subs \
-#      --embed-metadata \
-#      $2 \
-#    	$1
-#		if [[ $? -eq 0 ]]; then
-#      break
-#    fi
-#	done
-#}
-
-download_video () {
-	yt-dlp \
-  --sub-langs en,id,-live_chat \
-  --embed-subs \
-  --embed-metadata \
-  --live-from-start \
-  --output "%(uploader)s - %(title)s [%(id)s].%(ext)s"  \
-  $2 \
-  $1
-}
-
-cookies=""
 if [ -f cookies.txt ]; then
-  cookies="--cookies cookies.txt"
+  command_array+=('--cookies' 'cookies.txt')
+fi
+
+if [[ "$1" == '-l' ]]; then
+  command_array+=('--format' 'bv[height<=1080]+ba/b[height<=1080]')
+  shift
+  url="$@"
+else
+  url="$@"
 fi
 
 if [[ "$url" =~ ( |\') ]]; then
   arr=(${url})
   for each in "${arr[@]}"; do
-    download_video ${each} "${cookies}"
+    "${command_array[@]}" ${each}
   done
 else
-  download_video ${url} "${cookies}"
+  "${command_array[@]}" ${url}
 fi
