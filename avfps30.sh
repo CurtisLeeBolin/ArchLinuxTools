0f11ef60622656d2545b0caf4fac3ad140aa4a9e
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-09-22 12:43:33 -0500

    Add avfps30.sh

--- avfps30.sh
+++ avfps30.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-mkdir 0out_fps
+if [ ! -d '0out' ]; then
+  mkdir 0out
+fi
 
 for i in *.mp4; do
-  ffmpeg -discard nokey -i "${i}" -r 30 -codec copy 0out_fps/"${i%.*}.mkv"
+  ffmpeg -discard nokey -i "${i}" -r 30 -codec copy -y 0out/"${i%.*}.mkv"
+  if [ $? -eq 0 ]; then
+    rm "${i}"
+  fi
 done
