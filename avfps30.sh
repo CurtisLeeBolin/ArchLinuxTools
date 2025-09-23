33119f0e00267bec1d47815a95dfd1dd55eeec69
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-09-23 10:59:14 -0500

    Updates

--- avfps30.sh
+++ avfps30.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ ! -d '0out' ]; then
-  mkdir 0out
+dir_in='0in'
+dir_out='0out'
+
+if [ ! -d "${dir_out}" ]; then
+  mkdir "${dir_in}" "${dir_out}"
 fi
 
 for i in *.mp4; do
-  ffmpeg -discard nokey -i "${i}" -r 30 -codec copy -y 0out/"${i%.*}.mkv"
+  ffmpeg -discard nokey -i "${i}" -r 30 -codec copy -y "${dir_out}/${i%.*}.mkv"
   if [ $? -eq 0 ]; then
-    rm "${i}"
+    mv "${i}" "${dir_in}/${i}"
   fi
 done
