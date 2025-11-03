df628e872d4044afa8ae83318889b1afd9cd435c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-11-03 09:53:40 -0600

    Updated

--- nrsearch.sh
+++ nrsearch.sh
@@ -2,6 +2,6 @@
 
 query="$@"
 
-find $HOME/.tmp/nr/ -type d -iname "*${query}*"
+find $HOME/staging/nr/ -type d -iname "*${query}*"
 
-find $HOME/.tmp/nr/ -type f -name info -exec bash -c "if grep --ignore-case ${query} {} >/dev/null; then echo -e '{}'; fi" \;
+find $HOME/staging/nr/ -type f -name info -exec bash -c "if grep --ignore-case ${query} {} >/dev/null; then echo -e '{}'; fi" \;
