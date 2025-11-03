0388ecc6b3fbb61f18fa729a005c93d4e2870d6f
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-11-03 09:52:25 -0600

    Updated

--- avsearch.sh
+++ avsearch.sh
@@ -3,6 +3,9 @@
 file_type_array=(avi flv mov mp4 mpeg mpg ogg ogm ogv wmv m2ts mkv rmvb rm 3gp m4a 3g2 mj2 asf divx vob webm)
 
 
+path_to_search=$(realpath ${HOME}/{Videos,staging}/)
+
+
 print_help () {
   echo "avsearch - search for directories and files, giving info about video files"
   echo
@@ -30,14 +33,14 @@ get_regex_string () {
 
 
 search () {
-  #find "${HOME}"/{Videos,.tmp/saved}/ -type f -regextype posix-egrep -iregex "${regex_string}" -printf '%f\n' | sort
-  find "${HOME}"/{Videos,.tmp/saved}/ -type f -regextype posix-egrep -iregex "${regex_string}" | sort
+  #find ${path_to_search} -type f -regextype posix-egrep -iregex "${regex_string}" -printf '%f\n' | sort
+  find ${path_to_search} -type f -regextype posix-egrep -iregex "${regex_string}" | sort
 }
 
 
 search_verbose () {
   result_array=()
-  mapfile -d '' result_array < <(find "${HOME}"/{Videos,.tmp/saved}/ -type f -regextype posix-egrep -iregex "${regex_string}" -print0 2>/dev/null)
+  mapfile -d '' result_array < <(find ${path_to_search} -type f -regextype posix-egrep -iregex "${regex_string}" -print0 2>/dev/null)
   for result in "${result_array[@]}"; do
     echo "${result}"
     file_data_array=($(ls -lh --full-time "${result}"))
