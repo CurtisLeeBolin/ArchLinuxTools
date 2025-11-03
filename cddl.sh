865d4c1c7742c212107211f9e3a511b6163f1a3b
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-11-03 09:56:06 -0600

    Added update-cyberdrop-dl.sh and cddl.sh

---
+++ cddl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# cddl.sh
+
+command_array=(
+  'cyberdrop-dl'
+  '--download'
+  '--output-folder' './'
+)
+
+if [[ "$1" == '--images' ]]; then
+  shift
+  command_array+=('--exclude-audio')
+else
+  command_array+=(
+    '--exclude-images'
+    '--exclude-audio'
+  )
+fi
+
+"${command_array[@]}" ${@}
---
+++ update-cyberdrop-dl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# update-cyberdrop-dl.sh
+
+app='cyberdrop-dl'
+
+if [[ ! -f $(which git 2>/dev/null) ]]; then
+  sudo pacman -Sy --noconfirm --needed git
+fi
+
+if [ -d ~/.local/lib/"${app}"/ ]; then
+  rm -r ~/.local/lib/"${app}"
+fi
+
+python -m venv ~/.local/lib/"${app}"
+
+~/.local/lib/"${app}"/bin/python -m pip install --upgrade pip
+
+#~/.local/lib/"${app}"/bin/python -m pip install git+https://github.com/evan-sm/"${app}".git
+~/.local/lib/"${app}"/bin/python -m pip install "${app}[extra]"
+
+cp ~/.local/lib/"${app}"/bin/"${app}" ~/.local/bin/
