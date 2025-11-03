3b1344a44ee2a40c6c3b1ac0e0e4683000b2b3bf
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-11-03 09:54:32 -0600

    Updated

--- sdd.sh
+++ sdd.sh
@@ -4,13 +4,15 @@
 # sd <link>
 
 
+url=$(echo "${@}" | grep -Eo '(https?://[^[:space:]]+)')
+
 command_array_stream=(
   'streamlink'
   '--hls-live-restart'
   '--ffmpeg-start-at-zero'
   '--force'
   '--output' '{time:%Y%m%d%H%M%S} {author} - {title} [{id}].mp4'
-  "${1}"
+  "${url}"
   'best'
 )
 
@@ -20,7 +22,7 @@ command_array_clip=(
   '--ffmpeg-start-at-zero'
   '--force'
   '--output' '{author} - {title} [{id}].mp4'
-  "${1}"
+  "${url}"
   'best'
 )
 
@@ -32,14 +34,16 @@ get_stream () {
   fi
 }
 
-if [[ "${@}" =~ ( |\') ]]; then
-  arr=(${@})
-  for each in "${arr[@]}"; do
-    get_stream ${each}
-    max=60
-    min=15
-    sleep $(shuf -i $min-$max -n 1)
-  done
-else
-  get_stream ${@}
-fi
+#if [[ "${@}" =~ ( |\') ]]; then
+#  arr=(${@})
+#  for each in "${arr[@]}"; do
+#    get_stream ${each}
+#    max=60
+#    min=15
+#    sleep $(shuf -i $min-$max -n 1)
+#  done
+#else
+#  get_stream ${@}
+#fi
+
+get_stream "${url}"
